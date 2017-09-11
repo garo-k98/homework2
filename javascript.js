081d102cbd35247df2ae4605105fc629d7b5bd4f
garo-k98 <31818517+garo-k98@users.noreply.github.com>
2017-09-11 10:59:01 +0400

    homework2

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var x = 20;
+var y = 30;
+var z = x+y;
+
+document.getElementById("demo").innerHTML = z;
+
+var j = y-x;
+
+document.getElementById("demi").innerHTML = j;
+
+console.log(j+x);
+console.log(y*j);
